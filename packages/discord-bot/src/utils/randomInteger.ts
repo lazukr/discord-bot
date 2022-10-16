eefa2d1da52d7bf2ae1152c9efb325511d058c23
lazukr <peterh10201@gmail.com>
2022-10-15 21:36:17 -0700

    Restructured randomInteger to be more testable.

--- packages/discord-bot/src/utils/randomInteger.ts
+++ packages/discord-bot/src/utils/randomInteger.ts
@@ -1,3 +1,7 @@
-export const randomInteger = (min: number, max: number, rand: () => number = Math.random) => {
+export interface RandomIntegerGenerator {
+    (min: number, max: number, rand?: () => number): number;
+}
+
+export const randomInteger: RandomIntegerGenerator = (min, max, rand = Math.random) => {
     return Math.floor(rand() * (max - min) + min);
 };
