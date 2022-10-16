3faa2c49ec9590d21d8d4b7671a35571109e6196
lazukr <peterh10201@gmail.com>
2022-10-15 21:36:33 -0700

    Reworked choose command to be unit testable.

---
+++ packages/discord-bot/src/commands/generic/ChooseCommand.test.ts
@@ -0,0 +1,22 @@
+import { expect, test } from "@jest/globals";
+import { RandomIntegerGenerator } from "../../utils/randomInteger";
+import { choose } from "./ChooseCommand";
+
+test("Empty input does not return any result.", () => {
+    const args: string[] = [];
+    expect(choose(args)).toBe("No items to choose from.");
+});
+
+test("Same item return if only one item.", () => {
+    const args: string[] = ["test"];
+    expect(choose(args)).toBe("test");
+});
+
+test("Selects an item in the list.", () => {
+    const args: string[] = ["test,", "help"];
+    const randomResult: RandomIntegerGenerator = (min, max) => {
+        return 1;
+    };
+
+    expect(choose(args, randomResult)).toBe("help");
+});
