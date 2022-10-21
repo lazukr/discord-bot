bfe308cfac3ea1c47edf00f057ea78c3b75fe2d9
lazukr <peterh10201@gmail.com>
2022-10-20 22:42:49 -0700

    updated NovaSigCommand and added unit test case for parseSigArgs

---
+++ packages/discord-bot/src/commands/novaro/NovaSigCommand.test.ts
@@ -0,0 +1,59 @@
+import { expect, test, describe } from "@jest/globals";
+import { parseSigArgs, parseCharConfig, BG_MAX, POSE_MAX } from "./NovaSigCommand";
+
+describe("test parseSigArgs", () => {
+    test("name only.", () => {
+        const args = ["test"];
+        const [name, config] = parseSigArgs(args);
+        expect(name).toBe("test");
+        expect(config).toBe("");
+    });
+
+    test("config only.", () => {
+        const args = ["a/a"];
+        const [name, config] = parseSigArgs(args);
+        expect(name).toBe("");
+        expect(config).toBe("a/a");
+    });
+
+    test("name with space.", () => {
+        const args = ["test", "hello"];
+        const [name, config] = parseSigArgs(args);
+        expect(name).toBe("test_hello");
+        expect(config).toBe("");
+    });
+
+    test("name with config.", () => {
+        const args = ["test", "a/b"];
+        const [name, config] = parseSigArgs(args);
+        expect(name).toBe("test");
+        expect(config).toBe("a/b");
+    });
+
+    test("name with partial config (a/).", () => {
+        const args = ["test", "a/"];
+        const [name, config] = parseSigArgs(args);
+        expect(name).toBe("test");
+        expect(config).toBe("a/");
+    });
+
+    test("name with partial config (/a).", () => {
+        const args = ["test", "/a"];
+        const [name, config] = parseSigArgs(args);
+        expect(name).toBe("test");
+        expect(config).toBe("/a");
+    });
+
+    test("multi space name with config.", () => {
+        const args = ["hello", "world", "a/b"];
+        const [name, config] = parseSigArgs(args);
+        expect(name).toBe("hello_world");
+        expect(config).toBe("a/b");
+    });
+});
+
+describe("test parseCharConfig", () => {
+
+
+
+});
--- packages/discord-bot/src/commands/novaro/NovaSigCommand.ts
+++ packages/discord-bot/src/commands/novaro/NovaSigCommand.ts
@@ -3,16 +3,27 @@ import { Logger } from "../../Logger";
 import { DiscordBot } from "../../DiscordBot";
 import { randomInteger } from "../../utils/randomInteger";
 
-const POSE_MAX = 12;
-const BG_MAX = 10;
+export const POSE_MAX = 12;
+export const BG_MAX = 10;
 
+const configRegex = /(\d*)\/(\d*)/;
 export class NovaSigCommand extends Command {
     constructor(client: DiscordBot) {
         super("novasig", (msg, args) => {
             Logger.log(`NovaSig Command ran from ${msg.author} with args: ${args}`);
-            const [name, config] = args;
+
+            if (args.length === 0) {
+                return;
+            }
+
+            const [name, config] = parseSigArgs(args);
+
+            if (name === null) {
+                // handle this
+            }
+
             const [bg, pose] = parseCharConfig(config);
-            
+            Logger.log(`NovaSig name: ${name}, bg: ${bg}, pose: ${pose}`);
         }, {
             description: "Gets the signature of a character from NovaRO.",
             fullDescription: "Gets the signature of a character from NovaRO.",
@@ -22,7 +33,15 @@ export class NovaSigCommand extends Command {
     }    
 }
 
-const parseCharConfig = (config: string): [number, number] => {
+export const parseSigArgs = (args: string[]): [string, string] => {
+    const lastArg = args[args.length - 1];
+    const match = lastArg!.match(configRegex);
+    const config =  match ? lastArg : "";
+    const name = match ? args.slice(0, -1).join("_") : args.join("_");
+    return [name, config];
+};
+
+export const parseCharConfig = (config: string): [number, number] => {
     // empty string or null or undefined
     if (!config) {
         // return two random numbers as results
