9434e71a53365ce32d435cddbde01a6edd2155f9
lazukr <peterh10201@gmail.com>
2023-04-13 22:28:19 -0700

    added timestamp to logging for file and error

--- src/logging/WinstonLogger.ts
+++ src/logging/WinstonLogger.ts
@@ -3,6 +3,7 @@ import DailyRotate from "winston-daily-rotate-file";
 import { Logger } from "./Logger.js";
 import { LoggerConfig } from "./LoggerConfig.js";
 
+const { combine, json, timestamp } = format;
 export class WinstonLogger implements Logger {
     private logger: Winston.Logger;
     constructor(config: LoggerConfig) {
@@ -14,6 +15,12 @@ export class WinstonLogger implements Logger {
 
         if (config.logToError) {
             this.logger.add(new Winston.transports.File({
+                format: combine(
+                    timestamp({
+                        format: "YYYY-MM-DD HH:mm:ss.SSS",
+                      }),
+                    json(),
+                ),
                 filename: "errors.log",
                 level: "error",
                 dirname: "./logs/",
@@ -22,6 +29,12 @@ export class WinstonLogger implements Logger {
 
         if (config.logToFile) {
             this.logger.add(new DailyRotate({
+                format: combine(
+                    timestamp({
+                        format: "YYYY-MM-DD HH:mm:ss.SSS",
+                      }),
+                    json(),
+                ),
                 level: "info",
                 filename: "%DATE%.log",
                 datePattern: "YYYY-MM-DD",
