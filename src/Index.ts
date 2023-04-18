f0ecd2ac6e08498fe82a7274da43971e797ecebe
lazukr <peterh10201@gmail.com>
2023-04-17 22:31:17 -0700

    added process.on for potentially uncaught things

--- src/Index.ts
+++ src/Index.ts
@@ -1,6 +1,17 @@
 import { DiscordBot } from "./DiscordBot.js";
 import { config } from "./configuration/Config.js";
 import { Scheduler } from "./Scheduler.js";
+import { BotLogger } from "./BotLogger.js";
 const bot = new DiscordBot(config);
 Scheduler.init(bot);
 bot.start();
+
+process.on("uncaughtException", (err, origin) => {
+    BotLogger.error(err.message + origin);
+});
+
+process.on("unhandledRejection", (reason, promise) => {
+    const error = reason as Error;
+    const prom = promise.toString();
+    BotLogger.error(error.message + prom);
+});
